5b48592691b73976f8da11739606939cfda9d888
joaomarcos9741 <joaommarcosc@gmail.com>
2024-08-09 16:45:18 -0300

    page 3

--- src/components/Loading/index.ts
+++ src/components/Loading/index.ts
@@ -2,14 +2,10 @@ import React from "react";
 import LottieView from "lottie-react-native";
 
 export function Loading() {
-    return 
-    
-        <LottieView
-            source={require('../../lotties/loading.json')}
-            style={{ width: "100%", height: "100%" }}
-            autoPlay
-            loop
-
-        />
-    
+    return <LottieView
+        source ={require('../../lotties/foquete.json')}
+        style = {{width: "100%", height: "100%"}}
+        autoPlay
+        loop
+    />
 }
